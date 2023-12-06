142c9581b97bf90120596194beaf6fe996d822cb
Chiggy-Playz <garg.chirag2004@gmail.com>
2023-12-06 23:45:54 +0530

    2023 - Day 5 Part 2 still doesnt work! Not gonna waste time on it

--- 2023/day5.py
+++ 2023/day5.py
@@ -4,7 +4,6 @@ from collections import defaultdict
 from typing import Generator
 
 input = get_input(__file__, lines=False)
-part1 = True
 
 raw_initial_seeds, *raw_categories = input.split("\n\n")
 initial_seeds = [int(num) for num in re.findall(r"\d+", raw_initial_seeds)]
@@ -19,44 +18,74 @@ for raw_category in raw_categories:
     ]
 
 
-def get_seeds(initial_seeds: list[int], part1: bool) -> Generator[int, None, None]:
-    if part1:
-        for seed in initial_seeds:
-            yield seed
-    else:
-        for seed_start, count in zip(initial_seeds[::2], initial_seeds[1::2]):
-            for seed in range(seed_start, seed_start + count):
-                yield seed
+def part1():
+    seed_to_location: dict[int, int] = {}
+
+    for seed in initial_seeds:
+        previous_category_destination = seed
+        for category, category_ranges in categories.items():
+            for dest_start, source_start, delta in category_ranges:
+                if (previous_category_destination >= (source_start + delta)) or (
+                    previous_category_destination < source_start
+                ):
+                    continue
+
+                previous_category_destination = (
+                    dest_start - source_start + previous_category_destination
+                )
+                break
+        seed_to_location[seed] = previous_category_destination
+
+    return min(seed_to_location.values())
 
 
-processed: dict[str, dict[int, int]] = defaultdict(dict)
-seed_to_location: dict[int, int] = {}
-
-for seed in get_seeds(initial_seeds, part1):
-    previous_category_destination = seed
+def part2():
+    # ex, seed-to-soil: [ (98, 99, 50, 51), (50, 97, 52, 99) ]
+    ranges: dict[str, list[tuple[int, int, int, int]]] = defaultdict(list)
+    loc = float("inf")
     for category, category_ranges in categories.items():
-        if (category in processed) and previous_category_destination in processed[
-            category
-        ]:
-            previous_category_destination = processed[category][
-                previous_category_destination
-            ]
-            continue
-
-        processed[category][
-            previous_category_destination
-        ] = previous_category_destination
-
+        start_min = float("inf")
+        start_max = 0
+        dest_min = float("inf")
+        dest_max = 0
+        count = 0
         for dest_start, source_start, delta in category_ranges:
-            if (previous_category_destination >= (source_start + delta)) or (
-                previous_category_destination < source_start
-            ):
-                continue
-            dest = dest_start - source_start + previous_category_destination
-            processed[category][previous_category_destination] = dest
-            previous_category_destination = dest
-            break
+            source_end = source_start + delta - 1
+            dest_end = dest_start + delta - 1
 
-    seed_to_location[seed] = previous_category_destination
+            ranges[category].append((source_start, source_end, dest_start, dest_end))
 
-print(min(seed_to_location.values()))
+    loc = float("inf")
+    for seed_start, seed_count in zip(initial_seeds[::2], initial_seeds[1::2]):
+        seed_count -= 1
+        seed_end = seed_start + seed_count
+        previous_range = (seed_start, seed_end)
+        for category, category_ranges in ranges.items():
+            start_matched, end_matched = False, False
+            old = previous_range
+            for source_start, source_end, dest_start, dest_end in category_ranges:
+                if (not start_matched) and (
+                    source_start <= previous_range[0] <= source_end
+                ):
+                    delta = previous_range[0] - source_start
+                    previous_range = (
+                        dest_start + delta,
+                        previous_range[1],
+                    )
+                    start_matched = True
+                if (not end_matched) and (
+                    source_start <= previous_range[1] <= source_end
+                ):
+                    delta = previous_range[1] - source_start
+                    previous_range = (
+                        previous_range[0],
+                        dest_start + delta if ((source_start <= previous_range[0] <= source_end) or previous_range[0] > source_end) else dest_start,
+                    )
+                    end_matched = True
+
+        loc = min(loc, previous_range[0], previous_range[1])
+    return loc
+
+
+print(part1())
+print(part2())  # doesnt work :)
