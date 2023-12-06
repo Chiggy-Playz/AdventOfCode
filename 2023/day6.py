b92177ba11d258d9ed6ed0aa1f7746ae15125789
Chiggy-Playz <garg.chirag2004@gmail.com>
2023-12-06 23:52:17 +0530

    2023 - Day 6

---
+++ 2023/day6.py
@@ -0,0 +1,19 @@
+from input_manager import get_input
+import re
+
+input = get_input(__file__)
+part1 = True
+time, distance = (
+    [int(num) for num in re.findall(r"\d+", line.replace(" ", " " if part1 else ""))]
+    for line in input
+)
+
+prod = 1
+for race_time, race_distance in zip(time, distance):
+    ways = 0
+    for i in range(race_time + 1):
+        if i * (race_time - i) > race_distance:
+            ways += 1
+    prod *= ways
+
+print(prod)
