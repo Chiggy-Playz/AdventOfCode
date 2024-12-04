3c7c074a98cceae6adef3f8008a6455b5a7a442b
Chiggy-Playz <garg.chirag2004@gmail.com>
2024-12-04 14:07:56 +0530

    Make get_input better

--- utils/input_manager.py
+++ utils/input_manager.py
@@ -1,6 +1,15 @@
+import inspect
 from os.path import dirname, basename, join
-from typing import overload, Literal
+from typing import Optional, overload, Literal
 
+
+@overload
+def get_input(full_path: None = None, lines: Literal[True] = ...) -> list[str]:
+    ...
+
+@overload
+def get_input(full_path: None = None, lines: Literal[False] = ...) -> str:
+    ...
 
 @overload
 def get_input(full_path: str, lines: Literal[True] = ...) -> list[str]:
@@ -12,7 +21,8 @@ def get_input(full_path: str, lines: Literal[False]) -> str:
     ...
 
 
-def get_input(full_path: str, lines=True):
+def get_input(full_path: Optional[str] = None, lines=True):
+    full_path = full_path or inspect.stack()[1].filename
     with open(
         join(dirname(full_path), "inputs", basename(full_path).split(".")[0] + ".txt")
     ) as f:
