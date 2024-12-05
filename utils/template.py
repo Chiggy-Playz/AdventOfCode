a0b0b1622ef8118585bbb12de0431f3482bcbda7
Chiggy-Playz <garg.chirag2004@gmail.com>
2024-12-05 13:54:36 +0530

    Add benchmark to template

--- utils/template.py
+++ utils/template.py
@@ -1,16 +1,21 @@
+from typing import TypeVar
+
 from utils.input_manager import get_input
 from utils.timing import timeit
+from timeit import timeit as benchmark
+
+InputType = TypeVar("InputType", str, list[str])
 
 
-def part1(lines: list[str]):
+def part1(lines: InputType):
     pass
 
 
-def part2(lines: list[str]):
+def part2(lines: InputType):
     pass
 
-def main():
-    lines = get_input(testing=True)
+
+def main(lines: InputType):
     with timeit("Part 1"):
         print(part1(lines))
 
@@ -19,4 +24,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    lines = get_input(testing=True)
+    main(lines)
+
+    # Benchmark:
+    # print(benchmark(lambda: main(lines)))
