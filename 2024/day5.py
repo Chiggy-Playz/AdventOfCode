d8cc0f1838f83c33aaa033a9ff841abf3d86bf43
Chiggy-Playz <garg.chirag2004@gmail.com>
2024-12-05 13:45:40 +0530

    2024 Day 5 merge parts

--- 2024/day5.py
+++ 2024/day5.py
@@ -2,16 +2,7 @@ from utils.input_manager import get_input
 from utils.timing import timeit
 
 
-def part1(lines: str):
-    rules_raw, updates = lines.split("\n\n")
-    rules: dict[int, list[int]] = {}
-    inv_rules: dict[int, list[int]] = {}
-    for rule in rules_raw.splitlines():
-        k, v = int(rule.split("|")[0]), int(rule.split("|")[1])
-        rules[k] = rules.get(k, []) + [v]
-        inv_rules[v] = inv_rules.get(v, []) + [k]
-
-    updates = [list(map(int, line.split(","))) for line in updates.splitlines()]
+def part1(rules: dict[int, list[int]], updates: list[list[int]]):
     safe = 0
     for update in updates:
         for i, num in enumerate(update):
@@ -19,20 +10,10 @@ def part1(lines: str):
                 break
         else:
             safe += update[len(update) // 2]
-
     return safe
 
 
-def part2(lines: str):
-    rules_raw, updates = lines.split("\n\n")
-    rules: dict[int, list[int]] = {}
-    inv_rules: dict[int, list[int]] = {}
-    for rule in rules_raw.splitlines():
-        k, v = int(rule.split("|")[0]), int(rule.split("|")[1])
-        rules[k] = rules.get(k, []) + [v]
-        inv_rules[v] = inv_rules.get(v, []) + [k]
-
-    updates = [list(map(int, line.split(","))) for line in updates.splitlines()]
+def part2(rules: dict[int, list[int]], updates: list[list[int]]):
     incorrect = 0
     for update in updates:
         for i, num in enumerate(update):
@@ -46,7 +27,7 @@ def part2(lines: str):
         for i in range(len(update) - 1):
             for j in range(i + 1, len(update)):
                 # Find if i and j are already in correct order
-                if update[i] in rules.get(update[j], []) or update[j] in inv_rules.get(update[i], []):
+                if update[i] in rules.get(update[j], []):
                     update[i], update[j] = update[j], update[i]
 
         incorrect += update[len(update) // 2]
@@ -54,10 +35,21 @@ def part2(lines: str):
     return incorrect
 
 
-if __name__ == "__main__":
+def main():
     lines = get_input(testing=False, lines=False)
+    rules_raw, updates = lines.split("\n\n")
+    rules: dict[int, list[int]] = {}
+    for rule in rules_raw.splitlines():
+        k, v = int(rule.split("|")[0]), int(rule.split("|")[1])
+        rules[k] = rules.get(k, []) + [v]
+
+    updates = [list(map(int, line.split(","))) for line in updates.splitlines()]
     with timeit("Part 1"):
-        print(part1(lines))
+        print(part1(rules, updates))
 
     with timeit("Part 2"):
-        print(part2(lines))
+        print(part2(rules, updates))
+
+
+if __name__ == "__main__":
+    main()
