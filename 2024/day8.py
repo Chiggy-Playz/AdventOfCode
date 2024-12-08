35df4fb012ed78ced0a46ab33b6feaabc78f919d
Chiggy-Playz <garg.chirag2004@gmail.com>
2024-12-08 16:10:26 +0530

    Solve both parts in one go and add benchmarks

--- 2024/day8.py
+++ 2024/day8.py
@@ -1,14 +1,10 @@
-# from utils.types import InputType
-from typing import TypeAlias, TypeVar
-
 from utils.input_manager import get_input
 from utils.stats import benchmark
 from utils.timing import timeit
 from utils.types import InputType, Point
 
 
-def part1(lines: InputType):
-
+def solver(lines: InputType):
     antennas: dict[str, list[Point]] = {}
 
     for y in range(len(lines)):
@@ -16,61 +12,44 @@ def part1(lines: InputType):
             if lines[y][x] not in ".#":
                 antennas[lines[y][x]] = antennas.get(lines[y][x], []) + [(x, y)]
 
-    antinodes = set()
+    antinodes1 = set()
+    antinodes2 = set()
 
     for _, positions in antennas.items():
         for first in positions:
             for second in positions:
                 if first == second:
                     continue
-
-                difference = map(lambda x: x[1] - x[0], zip(first, second))
-                nx, ny = map(sum, zip(difference, second))
-                if not (0 <= nx < len(lines) and 0 <= ny < len(lines)):
-                    continue
-
-                antinodes.add((nx, ny))
-
-    return len(antinodes)
-
-
-def part2(lines: InputType):
-    antennas: dict[str, list[Point]] = {}
-
-    for y in range(len(lines)):
-        for x in range(len(lines[y])):
-            if lines[y][x] not in ".#":
-                antennas[lines[y][x]] = antennas.get(lines[y][x], []) + [(x, y)]
-
-    antinodes = set()
-
-    for _, positions in antennas.items():
-        for first in positions:
-            for second in positions:
-                if first == second:
-                    continue
-                antinodes.add(first)
-                antinodes.add(second)
-
+                antinodes2.add(first)
+                antinodes2.add(second)
+                p1_solved = False
                 difference = tuple(map(lambda x: x[1] - x[0], zip(first, second)))
                 nx, ny = tuple(map(sum, zip(difference, second)))
 
-                while (0 <= nx < len(lines) and 0 <= ny < len(lines)):
-                    antinodes.add((nx, ny))
+                while 0 <= nx < len(lines) and 0 <= ny < len(lines):
+                    if not p1_solved:
+                        antinodes1.add((nx, ny))
+                        p1_solved = True
+
+                    antinodes2.add((nx, ny))
                     nx, ny = tuple(map(sum, zip(difference, (nx, ny))))
 
-    return len(antinodes)
-
+    return len(antinodes1), len(antinodes2)
 
 def main(lines: InputType):
-    with timeit("Part 1"):
-        print(part1(lines))
-
-    with timeit("Part 2"):
-        print(part2(lines))
+    with timeit("Both parts"):
+        print(solver(lines))
 
 
 if __name__ == "__main__":
     lines = get_input(testing=False)
     main(lines)
     benchmark(main, lines)
+    """
+    (367, 1285)
+    Both parts took 1.78 ms
+
+    --- Benchmarks ---
+    Number of runs: 100
+    Average per run: 1.17 ms
+    """
